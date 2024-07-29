568ac951246e81c226ce545cc143bae4fa9d699c
Ben Schroeter <benjschroeter@gmail.com>
2024-07-29 10:56:44 +1000

    Updated to state tests to work in-memory for true temporary testing

--- tests/test_state.py
+++ tests/test_state.py
@@ -3,37 +3,40 @@ from pathlib import Path
 
 import pytest
 from benchcab.utils.state import State, StateAttributeError
+from tempfile import TemporaryDirectory
 
 
-@pytest.fixture()
-def state():
-    """Return a State object."""
-    return State(state_dir=Path("my_state"))
-
-
-def test_state_is_set(state):
+def test_state_is_set():
     """Success case: test state is set."""
-    state.set("foo")
-    assert state.is_set("foo")
+    with TemporaryDirectory() as tmp_dir:
+        state = State(state_dir=Path(tmp_dir))
+        state.set("foo")
+        assert state.is_set("foo")
 
 
-def test_state_reset(state):
+def test_state_reset():
     """Success case: test state is reset."""
-    state.set("foo")
-    state.reset()
-    assert not state.is_set("foo")
+    with TemporaryDirectory() as tmp_dir:
+        state = State(state_dir=Path(tmp_dir))
+        state.set("foo")
+        state.reset()
+        assert not state.is_set("foo")
 
 
-def test_state_get(state):
+def test_state_get():
     """Success case: test get() returns the most recent state attribute."""
-    state.set("foo")
-    # This is done so that time stamps can be resolved between state attributes
-    time.sleep(0.01)
-    state.set("bar")
-    assert state.get() == "bar"
+    with TemporaryDirectory() as tmp_dir:
+        state = State(state_dir=Path(tmp_dir))
+        state.set("foo")
+        # This is done so that time stamps can be resolved between state attributes
+        time.sleep(1)
+        state.set("bar")
+        assert state.get() == "bar"
 
 
-def test_state_get_raises_exception(state):
+def test_state_get_raises_exception():
     """Failure case: test get() raises an exception when no attributes are set."""
-    with pytest.raises(StateAttributeError):
-        state.get()
+    with TemporaryDirectory() as tmp_dir:
+        state = State(state_dir=Path(tmp_dir))
+        with pytest.raises(StateAttributeError):
+            state.get()
